3b50c59ea949b93d7548863f5982afeecf083300
Loke <pakyenloke@gmail.com>
2023-01-15 19:00:22 +0800

    debugged foo.r and adding changes to assignment 4

--- Assignment 4/foo.R
+++ Assignment 4/foo.R
@@ -1,19 +1,19 @@
 # Radius
 r <- 2
 
-# Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
-  3/4*pi*r^2
-}
 
-# Function to compute the volumes of the spheres with radius r, r^2 and
-r^3
+# Function to compute the volume of a sphere with radius r
+volume <- function(r) {
+  3/4*pi*(r^3)
+}
+volume(r)
+
+# Function to compute the volumes of the spheres with radius r, r^2 and r^3
 volume_vector <- function(r) {
-  r <- 22
-  for (r in 2:4){
-    volume(r)
+    for (i in c(1,2,3)){
+    print(paste("Sphere with radius ",r^i, " has volume: ", volume(r^i)))
   }
 }
-# Run volume_vector(r) and print the volumes of the spheres with radius
-r, r^2 and r^3
+
+# Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
 volume_vector(r)
